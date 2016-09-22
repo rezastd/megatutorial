7ace1eacc61aa6e5ccacd3d044f81482c3c82473
Reza <reza.setiadi@icehousecorp.com>
2016-09-22 15:52:16 +0700

    updated

--- satu/app/views.py
+++ satu/app/views.py
@@ -1,6 +1,9 @@
-from flask import render_template, flash, redirect
-from app import app
+from flask import render_template, flash, redirect, session, url_for, request, g
+from flask.ext.login import login_user logout_user, current_user, login_required
+from app import app, db, lm, oid
 from .forms import LoginForm
+from .models import User
+
 @app.route('/')
 @app.route('/index')
 def index():
@@ -19,9 +22,30 @@ def index():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if g.user is not None and g.user.is_authenticated:
+		return redirect(url_for('index'))
 	form = LoginForm()
 	if form.validate_on_submit():
-		flash('login requested for OpenId="%s", remember_me=%s' %
-			(form.openid.data, str(form.remember_me.data)))
-		return redirect('/index')
+		session ['remember_me'] = form.remember_me.data
+		return oid.try_login(form.openid.data, ask_for=['nickname', 'email'])
 	return render_template('login.html', title='sign in', form=form, providers=app.config['OPENID_PROVIDERS'])
+
+@oid.after_login
+def after_login(resp):
+	if resp.email is None or resp.email == "":
+		flash('invalid login. please try again.')
+		return redirect(url_for('login'))
+	user = User.query.filter_by(email=resp.email).first()
+	if user is None:
+		nickname = resp.nickname
+		if nickname is None or nickname == "":
+			nickname = resp.email.split('@')[0]
+		user = User(nickname=nickname, email=resp.email)
+		db.session.add(user)
+		db.session.commit()
+	remember_me = False
+	if 'remember_me' in session:
+		remember_me = session['remember_me']
+		session.pop('remember_me', None)
+	login_user(user, remember = remember_me)
+	return redirect(request.args.get('next') or url_for('index'))			

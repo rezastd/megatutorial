14fa7e14554ee49397ca4c2bb99d2bb25c9edf18
Reza <reza.setiadi@icehousecorp.com>
2016-09-22 09:35:36 +0700

    my tutorial

---
+++ satu/app/views.py
@@ -0,0 +1,27 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+@app.route('/')
+@app.route('/index')
+def index():
+	user = {'nickname': 'Miguel'}  # fake user
+	posts = [
+	{
+	'author':{'nickname': 'john'},
+	'body': 'beautiful day in Portland!'
+	},
+	{
+	'author':{'nickname': 'susan'},
+	'body': 'the avengers movie was so cool!'
+	}
+	]
+	return render_template("index.html", title='Home', user=user, posts=posts)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+	form = LoginForm()
+	if form.validate_on_submit():
+		flash('login requested for oOpenId="%s", remember_me=%s' %
+			(form.openid.data, str(form.remember_me.data)))
+		return redirect('/index')
+	return render_template('login.html', title='sign in', form=form)

782d7bde85836defd0708eedac113467217c0823
Reza <reza.setiadi@icehousecorp.com>
2016-09-22 10:54:43 +0700

    +db

--- satu/config.py
+++ satu/config.py
@@ -8,3 +8,8 @@ OPENID_PROVIDERS = [
     {'name': 'FLickr', 'url': 'http://www.flickr.com/<username>'},
     {'name': 'MyOpenID', 'url': 'https://www.myopenid.com'}
 ]
+
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
